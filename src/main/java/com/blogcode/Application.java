261837d098001d790bc58b84f0587bc68ea56318
frebigbird <frebigbird@gmail.com>
2018-06-22 13:03:17 +0900

    TEST

--- src/main/java/com/blogcode/Application.java
+++ src/main/java/com/blogcode/Application.java
@@ -3,7 +3,7 @@ package com.blogcode;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-/** TES*/
+/** TEST */
 @SpringBootApplication
 public class Application {
 
