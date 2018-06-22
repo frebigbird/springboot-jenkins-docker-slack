2364326e61be26a270577e251a693f616014da6d
frebigbird <frebigbird@gmail.com>
2018-06-22 11:49:54 +0900

    push test

--- src/main/java/com/blogcode/Application.java
+++ src/main/java/com/blogcode/Application.java
@@ -3,6 +3,7 @@ package com.blogcode;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/** TES*/
 @SpringBootApplication
 public class Application {
 
